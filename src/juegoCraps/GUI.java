053a952ec5524fbb8901457796dd4bfa5b28dc4b
Fabian Lopez <fabian.muriel@correounivalle.edu.co>
2021-12-05 22:27:07 -0500

    finished GUI sketch 1

--- src/juegoCraps/GUI.java
+++ src/juegoCraps/GUI.java
@@ -1,104 +1,121 @@
 package juegoCraps;
 
 import javax.swing.*;
-import javax.swing.border.TitledBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ * This class is used as View Craps Class
+ * @author Fabian-L Lopez-M fabian.muriel@correounivalle.edu.co
+ * @version v.1.0.0 date:03/12/2021
+ */
 public class GUI extends JFrame {
-    //atributos
-    private JButton miFoto, miHobby, misExpectativas;
-    private JPanel panelBotones, panelDatos;
-    private Header titulo;
-    private JLabel labelImagen;
-    private JTextArea textoExpectativas;
-    private Escucha escucha;
+    private static final String MENSAJE_INICIO = "Bienvenido a Craps \n"
+            + "Oprime el boton lanzar para iniciar el juego"
+            + "\nSi tu tito de salida es 7 u 11 ganas con Natural"
+            + "\nSi tu tiro de salida es 2, 3 u 12 pierdes con Craps"
+            + "\nSi sacas cualquier otro valor estableceras el punto"
+            + "\nEstado en punto podras seguir lanzando los dados"
+            + "\npero ahora ganaras si sacas nuevamente el valor del punto "
+            + "\nsin que previamente hayas sacado 7";
 
-    //metodos
+    private Header headerProyect;
+    private JLabel dado1, dado2;
+    private JButton lanzar;
+    private JPanel panelDados, panelResultados;
+    private ImageIcon imageDado;
+    private JTextArea mensajesSalida, resultadosDados;
+    private JSeparator separator;
+    private Escucha escucha;
+    private ModelCraps modelCraps;
+
+    /**
+     * Constructor of GUI class
+     */
     public GUI() {
         initGUI();
 
         //Configuracion base de la ventana
-        this.setTitle("Mi Presentacion");
-        this.setSize(600, 540);
-        this.setLocationRelativeTo(null);
+        this.setTitle("Juego Craps");
+        //this.setSize(600, 540);
+        this.pack();
+        this.setResizable(true);
         this.setVisible(true);
+        this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     private void initGUI() {
         //Definir Container y Layout del JFrame
-        //Crear objetos Escucha y Control
+        //Crear objetos Escucha o Control
         escucha = new Escucha();
+        modelCraps = new ModelCraps();
         //Configurar JComponent
-        titulo = new Header("Hola soy Fabian, oprime los botones para conocerme", Color.BLACK);
-        this.add(titulo, BorderLayout.NORTH);
+        headerProyect = new Header("Mesa Juego Craps", Color.BLACK);
+        this.add(headerProyect,BorderLayout.NORTH);
 
-        panelDatos = new JPanel();
-        panelDatos.setBorder(BorderFactory.createTitledBorder(null, "Un poco mas de mi...",
-                TitledBorder.CENTER, TitledBorder.DEFAULT_POSITION,
-                new Font("Calibri", Font.PLAIN, 20), Color.BLACK));
+        imageDado = new ImageIcon(getClass().getResource("/resources/dado.png"));
+        dado1 = new JLabel(imageDado);
+        dado2 = new JLabel(imageDado);
 
-        this.add(panelDatos, BorderLayout.CENTER);
+        lanzar = new JButton("lanzar");
+        lanzar.addActionListener(escucha);
 
-        miFoto = new JButton("Este soy yo");
-        miFoto.addActionListener(escucha);
-        miHobby = new JButton("Este es mi Hobby");
-        miHobby.addActionListener(escucha);
-        misExpectativas = new JButton("Creo que...");
-        misExpectativas.addActionListener(escucha);
+        panelDados = new JPanel();
+        panelDados.setPreferredSize(new Dimension(300, 180));
+        panelDados.setBorder(BorderFactory.createTitledBorder(" Tus Dados "));
+        panelDados.add(dado1);
+        panelDados.add(dado2);
+        panelDados.add(lanzar);
 
-        panelBotones = new JPanel();
-        panelBotones.add(miFoto);
-        panelBotones.add(miHobby);
-        panelBotones.add(misExpectativas);
+        this.add(panelDados,BorderLayout.CENTER);
 
-        this.add(panelBotones, BorderLayout.SOUTH);
+        mensajesSalida = new JTextArea(7, 31);
+        mensajesSalida.setText(MENSAJE_INICIO);
+        //mensajesSalida.setBorder(BorderFactory.createTitledBorder("Que debes hacer "));
+        JScrollPane scroll = new JScrollPane(mensajesSalida);
 
-        labelImagen = new JLabel();
-        textoExpectativas = new JTextArea(10,  12);
+        panelResultados = new JPanel();
+        panelResultados.setBorder(BorderFactory.createTitledBorder("Que debes hacer "));
+        panelResultados.add(scroll);
+        panelResultados.setPreferredSize(new Dimension(370, 180));
 
+
+        this.add(panelResultados,BorderLayout.EAST);
+
+        resultadosDados = new JTextArea(4,31);
+        separator = new JSeparator();
+        separator.setPreferredSize(new Dimension(320,7));
+        separator.setBackground(Color.BLUE);
     }
 
-    public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                GUI miGUIPresentacion = new GUI();
-            }
+    public static void main(String[] args){
+        EventQueue.invokeLater(() -> {
+            GUI miProyectGUI = new GUI();
         });
     }
 
     private class Escucha implements ActionListener {
-        private ImageIcon image;
 
         @Override
         public void actionPerformed(ActionEvent e) {
-            //JOptionPane.showMessageDialog(null, "Oprimiste boton");
-            panelDatos.removeAll();
-            if (e.getSource() == miFoto) {
-                image = new ImageIcon(getClass().getResource("/recursos/Yo.png"));
-                labelImagen.setIcon(image);
-                panelDatos.add(labelImagen);
-            } else {
-                if (e.getSource() == miHobby) {
-                    image = new ImageIcon(getClass().getResource("/recursos/Tocar guitarra.jpg"));
-                    labelImagen.setIcon(image);
-                    panelDatos.add(labelImagen);
+            modelCraps.calcularTiro();
+            int[] caras = modelCraps.getCaras();
+            imageDado = new ImageIcon(getClass().getResource("/resources/"+caras[0]+".png"));
+            dado1.setIcon(imageDado);
+            imageDado = new ImageIcon(getClass().getResource("/resources/"+caras[1]+".png"));
+            dado2.setIcon(imageDado);
+            modelCraps.determinarJuego();
 
-
-                }else{
-                    textoExpectativas.setText("Quiero aprender como funciona la programacion orientadada a objetos\n" +
-                            "Quiero aprender mas de Java\n" +
-                            "Quiero que mis conocimientos en la programacion sean mas amplios");
-                    textoExpectativas.setBackground(Color.darkGray);
-                    textoExpectativas.setForeground(Color.WHITE);
-                    textoExpectativas.setFont(new Font(Font.DIALOG, Font.ROMAN_BASELINE, 17));
-                    panelDatos.add(textoExpectativas);
-
-                }
-            }
+            panelResultados.removeAll();
+            panelResultados.setBorder(BorderFactory.createTitledBorder("Resultados "));
+            panelResultados.add(resultadosDados);
+            panelResultados.add(separator);
+            panelResultados.add(mensajesSalida);
+            resultadosDados.setText(modelCraps.getEstadoToString()[0]);
+            mensajesSalida.setRows(4);
+            mensajesSalida.setText(modelCraps.getEstadoToString()[1]);
             revalidate();
             repaint();
         }
